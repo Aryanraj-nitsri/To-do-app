2fee860b631264dbb49d5767a5bb0cc0cd0417a0
Aryan Raj <aryanraj123outlook@gmail.com>
2022-09-19 12:06:49 +0530

    unnecessary code deletion

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const Listsliders = document.querySelectorAll("#lists li img");
 const checkBoxes = document.querySelectorAll("#lists input ");
 const button = document.getElementById("button");
 const allTask = document.querySelector("#taskStates>span");
-let allLists=document.querySelectorAll("#lists li")
+let allLists = document.querySelectorAll("#lists li");
 
 // adding Event listeners
 
@@ -17,9 +17,9 @@ for (let list of Listsliders) {
 for (let checkbox of checkBoxes) {
   checkbox.addEventListener("click", handleCheck);
 }
-input.addEventListener("focus",handleFocus)
-input.addEventListener("focusout",handleFocus)
-// function to execute after submit 
+input.addEventListener("focus", handleFocus);
+input.addEventListener("focusout", handleFocus);
+// function to execute after submit
 function handleSubmit(e) {
   e.preventDefault();
   let value = input.value;
@@ -30,13 +30,10 @@ function handleSubmit(e) {
   input.value = "";
   const delteButton = document.querySelector(".newlist img");
   const newTask = document.querySelector(".newlist>div>input");
-   allLists = document.querySelectorAll("#lists li");
-  allTask.innerText=`All Task left: ${allLists.length} `
-  
-  
+  allLists = document.querySelectorAll("#lists li");
+  allTask.innerText = `All Task left: ${allLists.length} `;
   delteButton.addEventListener("click", handleClick);
   newTask.addEventListener("click", handleCheck);
-  
 }
 
 function handleClick(e) {
@@ -44,106 +41,89 @@ function handleClick(e) {
 }
 function handleCheck(e) {
   e.target.parentElement.parentElement.classList.toggle("completed");
-  e.target.setAttribute("checked","true")
 }
-function handleFocus(){
-console.log(button.style.display)
-if(button.style.display==="none" || !button.style.display ){
-
-    button.style.display="block"
-}
-else{
-    button.style.display="none"
-
+function handleFocus() {
+  console.log(button.style.display);
+  if (button.style.display === "none" || !button.style.display) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
 }
 
-
-}
-
-// footer functionality 
+// footer functionality
 const taskType = document.querySelectorAll("#taskClassification span");
+const slider = document.getElementById("slider");
 for (let element of taskType) {
-  element.addEventListener("click",handleTask)
+  element.addEventListener("click", handleTask);
 }
-const slider = document.getElementById("slider")
-let completedTasks = document.querySelectorAll(".completed");
-let Uncompleted = document.querySelectorAll("#lists li:not(.completed)");
 function handleTask(e) {
-
   if (e.target.innerText === "All") {
     slider.style.left = "0";
     slider.style.width = "1.3rem";
 
-    // for (let uncomplete of Uncompleted) {
-    //   uncomplete.style.display = "block"
-    // }
-    // for (let completedTask of completedTasks) {
-    //   completedTask.style.visibility="visible"
-    // }
     for (let list of allLists) {
-      list.style.display="flex"
+      list.style.display = "flex";
     }
     allTask.innerText = `All Task left: ${allLists.length} `;
     input.disabled = false;
-
-  }
-  else if (e.target.innerText === "Uncompleted") {
+  } else if (e.target.innerText === "Uncompleted") {
     slider.style.left = "14%";
     slider.style.width = "7rem";
     for (let list of allLists) {
-      list.style.display="none"
+      list.style.display = "none";
     }
-    const completedTasks = document.querySelectorAll(".completed")
+    const completedTasks = document.querySelectorAll(".completed");
     for (let list of allLists) {
-      // if(list.classList)
-      console.log(list.getAttribute("class"))
-      console.log(list.getAttribute("class"))
-      if (list.getAttribute("class") === null || list.getAttribute("class")==="newlist") {
-        list.style.display="flex"
+      if (
+        list.getAttribute("class") === null ||
+        list.getAttribute("class") === "newlist"
+      ) {
+        list.style.display = "flex";
       }
     }
-  allTask.innerText=`Uncompleted : ${allLists.length-completedTasks.length} `
+    allTask.innerText = `Uncompleted : ${
+      allLists.length - completedTasks.length
+    } `;
     input.disabled = true;
-  }
-  else if (e.target.innerHTML === "Completed") {
+  } else if (e.target.innerHTML === "Completed") {
     slider.style.left = "64%";
     slider.style.width = "6rem";
     for (let list of allLists) {
-      list.style.display="none"
+      list.style.display = "none";
     }
     const Uncompleted = document.querySelectorAll(".completed");
-    
+
     for (let list of allLists) {
-      // if(list.classList)
-      console.log(list.getAttribute("class"))
-      if (list.getAttribute("class") === "completed" || list.getAttribute("class")==="newlist completed") {
-        list.style.display="flex"
+      if (
+        list.getAttribute("class") === "completed" ||
+        list.getAttribute("class") === "newlist completed"
+      ) {
+        list.style.display = "flex";
       }
     }
-  allTask.innerText=`completed : ${Uncompleted.length} `
+    allTask.innerText = `completed : ${Uncompleted.length} `;
     input.disabled = true;
-  
   }
 }
 // task completing and clearing
 const checkAll = document.getElementById("checkAllTask");
 checkAll.addEventListener("click", handleCheckAll);
 function handleCheckAll() {
-const checkBoxes = document.querySelectorAll("#lists input ");
+  const checkBoxes = document.querySelectorAll("#lists input ");
 
   for (let checks of checkBoxes) {
     checks.checked = true;
   }
   for (let list of allLists) {
-    list.classList.add("completed")
+    list.classList.add("completed");
   }
 }
 const clearAll = document.getElementById("clearAll");
 clearAll.addEventListener("click", handleClear);
 function handleClear() {
   const checkedLists = document.querySelectorAll(".completed");
-  console.log(checkedLists)
   for (let checkedList of checkedLists) {
-    Lists.removeChild(checkedList)
+    Lists.removeChild(checkedList);
   }
 }
